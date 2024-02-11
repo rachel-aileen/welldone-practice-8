379f6a4e9d67d8ba938a2dd352d549316fe972f4
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-02-10 18:28:14 -0800

    nav practice 8.1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Navbar from './layouts/Navbar';
 
 
 function App() {
   return (
     <div>
+
+      <Navbar />
 
 
     </div>
---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+export default function Navbar() {
+
+
+
+
+    return (
+        <div className='navbar'>
+            <a className='logo' href="#home">Welldone</a>
+        </div>
+    )
+}
